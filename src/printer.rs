a62df059c8db7793046462a611f6f5e6c3de173d
Matthew J. Berger <matthewberger@nevada.unr.edu>
2022-07-18 22:16:54 -0600

    feat(printer): use private method to access stream

--- src/printer.rs
+++ src/printer.rs
@@ -7,6 +7,9 @@ use thiserror::Error;
 pub enum PrinterError {
     #[error("Failed to write bytes to printer.")]
     WriteError(#[from] async_std::io::Error),
+
+    #[error("Failed to read/write to the printer. The printer is disconnected.")]
+    NotConnected,
 }
 
 pub type Result<T, E = PrinterError> = std::result::Result<T, E>;
@@ -28,24 +31,29 @@ impl Printer {
     }
 
     pub async fn connect(&mut self) -> Result<()> {
+        if self.stream.is_some() {
+            self.disconnect().await?;
+        }
+
         let stream = TcpStream::connect(format!("{}:{}", self.ip, self.port)).await?;
         stream.set_nodelay(true)?;
         self.stream = Some(stream);
+
         Ok(())
     }
 
     pub async fn print_text(&mut self, text: &str) -> Result<()> {
-        if let Some(stream) = self.stream.as_mut() {
-            stream.write_all(text.as_bytes()).await?;
-        }
+        self.stream_mut()?.write_all(text.as_bytes()).await?;
         Ok(())
     }
 
     pub async fn disconnect(&mut self) -> Result<()> {
-        if let Some(stream) = self.stream.as_mut() {
-            stream.shutdown(Shutdown::Both)?;
-        }
+        self.stream_mut()?.shutdown(Shutdown::Both)?;
         self.stream = None;
         Ok(())
+    }
+
+    fn stream_mut(&mut self) -> Result<&mut TcpStream> {
+        self.stream.as_mut().ok_or(PrinterError::NotConnected)
     }
 }
