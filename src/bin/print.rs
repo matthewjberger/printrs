6bea0a1d745c7d170602d0ac9d410dddd75f80ce
Matthew J. Berger <matthewberger@nevada.unr.edu>
2022-07-18 22:29:01 -0600

    feat(printer): store placeholder text as bytes

--- src/bin/print.rs
+++ src/bin/print.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use printrs::Printer;
 
-const PLACEHOLDER_TEXT: &str = "
+const PLACEHOLDER_TEXT_BYTES: &[u8] = b"
 Lorem ipsum dolor sit amet, consectetur adipiscing elit ...
 ";
 
@@ -10,7 +10,7 @@ async fn main() -> Result<()> {
     let mut printer = Printer::new("192.168.0.172".to_string(), 9100);
 
     printer.connect().await?;
-    printer.send_raw_bytes(PLACEHOLDER_TEXT.as_bytes()).await?;
+    printer.send_raw_bytes(PLACEHOLDER_TEXT_BYTES).await?;
     printer.disconnect().await?;
 
     Ok(())
